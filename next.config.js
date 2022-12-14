39821e432cff45d9ff47e9d1e06a4df4a46027ef
emmanuel <emmanueldessaint27220@gmail.com>
2022-12-14 11:05:49 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const withPWA = require("next-pwa");
 
-module.exports = nextConfig
+// module.exports = withPWA({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//   },
+// });
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+ 
+const nextConfig = withPWA({
+  reactStrictMode: true,
+});
+ 
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+// import { Inter } from '@next/font/google'
+
+// If loading a variable font, you don't need to specify the font weight
+// const inter = Inter({ subsets: ['poppins'] })
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
